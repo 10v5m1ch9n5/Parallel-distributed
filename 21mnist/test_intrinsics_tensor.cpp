decd4e49c07b642961adfa0dbe2122e84456dfa0
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2023-02-09 16:43:45 +0900

    added output comments and renamed test_intrinsics_tensor.cpp

--- 21mnist/test_intrinsics_tensor.cpp
+++ 21mnist/test_intrinsics_tensor.cpp
@@ -7,14 +7,14 @@ int main() {
     tens1.init_const(1,1);
     tens2.init_const(1,2);
     
-    std::cout << _mm512_reduce_add_ps(_mm512_mul_ps(tens1.V(0,0,0,0),tens2.V(0,0,0,0))) << std::endl;
+    std::cout << _mm512_reduce_add_ps(_mm512_mul_ps(tens1.V(0,0,0,0),tens2.V(0,0,0,0))) << std::endl; // 32
     
     tensor<float,1,1,1,12> tens3; // vectorisation on an array with less than 16 elements
     tensor<float,1,1,1,12> tens4;
     tens3.init_const(1,1);
     tens4.init_const(1,2);
     
-    std::cout << _mm512_reduce_add_ps(_mm512_mul_ps(tens3.V(0,0,0,0),tens4.V(0,0,0,0))) << std::endl;
+    std::cout << _mm512_reduce_add_ps(_mm512_mul_ps(tens3.V(0,0,0,0),tens4.V(0,0,0,0))) << std::endl; // nan
     
     return 0;
 }
